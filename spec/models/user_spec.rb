3892cf657b46f054363a7510fd9a5248d7e28664
Oscar M Olivera <oscarmolivera@gmail.com>
2020-07-14 22:09:02 -0400

    UserModel-Spec: Tables and Asociations

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'table columns' do
+    it { is_expected.to have_db_column(:email) }
+    it { is_expected.to have_db_column(:encrypted_password) }
+  end
+
+  describe 'associations' do
+    it { is_expected.to have_many(:sales) }
+  end
 end
