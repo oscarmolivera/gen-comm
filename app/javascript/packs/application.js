c78b414edffe6d430c2d1ab3e703a17e0d7e2084
Oscar M Olivera <oscarmolivera@gmail.com>
2020-07-15 21:50:32 -0400

    AppLayout: Template JS files Webpack config close #25

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,6 +1,11 @@
     require("@rails/ujs").start()
     require("turbolinks").start()
     require("@rails/activestorage").start()
+    require("packs/demo/chart-area-demo")
+    require("packs/demo/chart-bar-demo")
+    require("packs/demo/chart-pie-demo")
+    require("packs/demo/datatables-demo")
+    require("packs/sb-admin-2")
     require("channels")    
     
     import 'bootstrap';
