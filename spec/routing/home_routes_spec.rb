603c596becf4a070571b20b1a9d79a77099331b6
Oscar M Olivera <oscarmolivera@gmail.com>
2020-07-15 20:41:20 -0400

    HomesController-Spec: Routing Spec close: 27

---
+++ spec/routing/home_routes_spec.rb
@@ -0,0 +1,7 @@
+require 'rails_helper'
+
+RSpec.describe 'HOME Routes', type: :routing do
+  it 'ROOT_PATH should route to Index' do
+    expect(get: root_path).to route_to('homes#index')
+  end
+end
