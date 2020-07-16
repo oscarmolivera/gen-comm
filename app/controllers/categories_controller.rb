508f0812578178afdd08a8169501a6aaba2fcf06
Oscar M Olivera <oscarmolivera@gmail.com>
2020-07-16 16:34:26 -0400

    CategoryController: create close #50

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -6,4 +6,16 @@ class CategoriesController < ApplicationController
   def new
     @category = Category.new
   end
+
+  def create
+    @category = Category.new(category_params)
+    respond_to do |format|
+      if @category.save
+        format.js
+      else
+        format.js { render :new, status: :Not_Acceptable }
+      end
+    end
+  end
+
 end
