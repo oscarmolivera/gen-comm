cd56e3700fbc8c744e8256d8db831cca5f73ee89
Oscar M Olivera <oscarmolivera@gmail.com>
2020-07-15 20:28:24 -0400

    UserModel-Spec: Add column "name" close #34

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,6 +4,7 @@ RSpec.describe User, type: :model do
   describe 'table columns' do
     it { is_expected.to have_db_column(:email) }
     it { is_expected.to have_db_column(:encrypted_password) }
+    it { is_expected.to have_db_column(:name) }
   end
 
   describe 'associations' do
