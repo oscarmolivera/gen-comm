29ae7e318e0071bcd4b27ca26c53d16927db6903
Oscar M Olivera <oscarmolivera@gmail.com>
2020-07-16 18:43:23 -0400

    CategoryViews: Seed file category close #68

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,26 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+User.create!(
+  first_name: 'Oscar',
+  last_name: 'Olivera',
+  email: 'oscarmolivera@gmail.com',
+  password: '123456',
+  password_confirmation: '123456'
+)
+puts 'My User created!'
+User.create!(
+  first_name: 'Ederson',
+  last_name: 'Floriano',
+  email: 'ederson@smtm.co',
+  password: 'smtm1234',
+  password_confirmation: 'smtm1234'
+)
+puts 'Ederson TestUser!'
+
+12.times do
+  Category.create!(
+    name:  Faker::Lorem.word,
+    description: Faker::Lorem.sentence(word_count: 3, supplemental: true, random_words_to_add: 4)
+  )
+end
+puts '12 Dummy Categories created!'
