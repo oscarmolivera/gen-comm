b557b31896836b46209a2f1af58afe090491d331
Oscar M Olivera <oscarmolivera@gmail.com>
2020-07-19 12:59:10 -0400

    CategoriesView: Modal New fix close# 71

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,4 +1,5 @@
 class CategoriesController < ApplicationController
+  skip_before_action :verify_authenticity_token, only: :create
   before_action :set_category, only: %i[edit update destroy]
   def index
     @categories = Category.all
