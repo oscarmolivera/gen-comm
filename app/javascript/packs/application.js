225d0cfcecb9b6874171e1a31c22320ac81a872e
Oscar M Olivera <oscarmolivera@gmail.com>
2020-07-19 11:50:55 -0400

    Views Corrections

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,11 +1,12 @@
     require("@rails/ujs").start()
     require("turbolinks").start()
     require("@rails/activestorage").start()
-    require("channels")    
+    require("channels") 
 
     require("packs/sb-admin-2")
     
     import 'bootstrap';
+    import "chart.js";
     import "@fortawesome/fontawesome-free/js/all";
     import './stylesheets/application.scss';
     global.$ = jQuery;
