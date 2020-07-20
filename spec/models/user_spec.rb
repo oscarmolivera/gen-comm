5d8566fd3700e2d85f6058ea55d8153088a86651
Oscar M Olivera <oscarmolivera@gmail.com>
2020-07-19 23:51:07 -0400

    RSpec update fixes. close #81

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,7 +4,8 @@ RSpec.describe User, type: :model do
   describe 'table columns' do
     it { is_expected.to have_db_column(:email) }
     it { is_expected.to have_db_column(:encrypted_password) }
-    it { is_expected.to have_db_column(:name) }
+    it { is_expected.to have_db_column(:first_name) }
+    it { is_expected.to have_db_column(:last_name) }
   end
 
   describe 'associations' do
