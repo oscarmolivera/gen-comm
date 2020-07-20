a98fb4f0ea491860172b473af2e771b669781fb9
Oscar M Olivera <oscarmolivera@gmail.com>
2020-07-19 21:28:09 -0400

    CategoryViews: datatables improvement close #60

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,10 +2,14 @@
     require("turbolinks").start()
     require("@rails/activestorage").start()
     require("channels") 
+    require('datatables.net-bs4')
     
     import "bootstrap";
     import "chart.js";
     import "../src/font-awesome";
+    import "../src/category";
     import "../src/sb-admin-2";
     import "./stylesheets/application.scss";
+    
+    import $ from 'jquery';
     global.$ = jQuery;
