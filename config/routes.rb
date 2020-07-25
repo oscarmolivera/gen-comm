4b15407461ae4783083065f979588b6137f39ba4
Oscar M Olivera <oscarmolivera@gmail.com>
2020-07-24 20:05:56 -0400

    ClientRoutes: Route & Client close #86

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   devise_for :users
 
   resources :categories
+  resources :clients
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get 'shared/tables', controller: :shared, action: :tables
 end
