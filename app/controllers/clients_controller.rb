51f8f64233f4a033a3d2249c4bf18e9082b9f362
Oscar M Olivera <oscarmolivera@gmail.com>
2020-07-24 21:36:31 -0400

    ClientsController: Create close #95

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -1,4 +1,5 @@
 class ClientsController < ApplicationController
+  skip_before_action :verify_authenticity_token, only: %i[create]
   def index
     @clients = Client.all
   end
@@ -6,5 +7,22 @@ class ClientsController < ApplicationController
   def new
     @client = Client.new
   end
+
+  def create
+    @client = Client.new(client_params)
+    respond_to do |format|
+      if @client.save
+        format.js
+      else
+        format.js { render :new, status: :method_not_allowed }
+      end
+    end
+  end
+
+  private
+
+  def client_params
+    params.require(:client).permit(:name, :email, :address, :telephone)
+  end
 end
-                                       
+                                      
