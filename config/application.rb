c387aead625b9ae599d26335fd04ce4c83820042
Oscar M Olivera <oscarmolivera@gmail.com>
2020-07-25 16:48:02 -0400

    AppI18N: App controller config #112

--- config/application.rb
+++ config/application.rb
@@ -31,5 +31,7 @@ module GenComm
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+    config.i18n.available_locales = [:es, :en] 
+    config.i18n.default_locale = :es
   end
 end
