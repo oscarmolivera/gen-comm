8277ba6c3317f365ccc2cb3fb4679c4d2813ac30
Oscar M Olivera <oscarmolivera@gmail.com>
2020-07-24 20:51:59 -0400

    ClientSpec: Faker fix

--- spec/factories/client.rb
+++ spec/factories/client.rb
@@ -5,7 +5,7 @@ FactoryBot.define do
   factory :client do |client|
     client.name { Faker::Name.name_with_middle }
     client.email { Faker::Internet.email }
-    client.adress { Faker::Address.full_address }
-    client.telefone { Faker::PhoneNumber.phone_number_with_country_code}
+    client.address { Faker::Address.full_address }
+    client.telephone { Faker::PhoneNumber.phone_number_with_country_code}
   end
 end
