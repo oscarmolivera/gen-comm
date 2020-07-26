d94edbbf5789435b000dcef6625b01d60c3bb23a
Oscar M Olivera <oscarmolivera@gmail.com>
2020-07-26 00:10:23 -0400

    AppI18N: Language Scope(EN ES) close #115

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
-  root to: 'homes#index'
-  devise_for :users
+  scope '(:locale)', locale: /en|es/ do
+    root to: 'homes#index'
+    devise_for :users
 
-  resources :categories
-  resources :clients
+    resources :categories
+    resources :clients
 
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get 'shared/tables', controller: :shared, action: :tables
+    get 'shared/tables', controller: :shared, action: :tables
+
+  end
 end
