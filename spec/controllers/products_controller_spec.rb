8ee66903bcfb8e2c19d69de21df30c98e05d51a9
Oscar M Olivera <oscarmolivera@gmail.com>
2020-08-11 16:44:21 -0400

    ProductsController: NEW SPecs

--- spec/controllers/products_controller_spec.rb
+++ spec/controllers/products_controller_spec.rb
@@ -47,5 +47,43 @@ RSpec.describe ProductsController, type: :controller do
       end
     end
   end
+
+  describe 'Get #New' do
+    subject {get :new, xhr: true}
+    context 'when user IS logged in' do
+      let(:product) { create(:product) }
+      let(:user) { create(:user) }
+      before do
+        sign_in(user)
+        subject
+      end
+      it 'generate a new product' do
+        expect(assigns(:product)).to be_a_new(Product)
+      end
+
+      it 'renders the new template' do
+        expect(response).to render_template(:new)
+      end
+  
+      it 'rendes template OK' do
+        expect(response).to have_http_status(200)
+      end
+    end
+
+    context 'when NO user is logged in' do
+      before { subject }
+      it 'does not assign @product' do
+        expect(assigns(:product)).to eq(nil)
+      end
+
+      it 'does not render the new template' do
+        expect(response).not_to render_template(:new)
+      end
+
+      it 'response code:Unauthorized' do
+        expect(response).to have_http_status(401)
+      end
+    end
+  end
 end
  
