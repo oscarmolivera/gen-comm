fc87e7466b477fbba56482a1065b94bea34c9568
Oscar M Olivera <oscarmolivera@gmail.com>
2020-08-11 18:52:14 -0400

    ProductsController:  EDIT close #160

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,8 @@
 class ProductsController < ApplicationController
   skip_before_action :verify_authenticity_token, only: %i[create]
-
+  before_action :set_product, only: %i[edit]
+  before_action :set_category, only: %i[edit]
+  before_action :set_supplier, only: %i[edit]
   def index
     @products = Product.all
   end
@@ -20,9 +22,23 @@ class ProductsController < ApplicationController
     end
   end
 
+  def edit; end
+
   private
 
   def product_params
     params.require(:product).permit(:name, :description, :existence, :price, :image, :category_id, :supplier_id)
+  end
+
+  def set_product
+    @product = Product.find(params[:id])
+  end
+
+  def set_category
+    @category = Category.all
+  end
+
+  def set_supplier
+    @supplier = Supplier.all
   end
 end
