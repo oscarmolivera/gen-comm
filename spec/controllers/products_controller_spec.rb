88195a93c73f20a5d8b5d0deb4df5f22f015d26c
Oscar M Olivera <oscarmolivera@gmail.com>
2020-08-10 23:02:23 -0400

    ProductsController-Spec: INDEX

---
+++ spec/controllers/products_controller_spec.rb
@@ -0,0 +1,51 @@
+require 'rails_helper'
+
+RSpec.describe ProductsController, type: :controller do
+  describe 'GET #Index' do
+    context 'when user IS logged in' do
+      let(:user) { create(:user) }
+      before do
+        sign_in(user)
+        get :index
+      end
+
+      context 'and some products are present' do
+        let!(:product) { create(:product) }
+
+        it 'assigns @products' do
+          expect(assigns(:products)).to eq([product])
+        end
+
+        it 'renders the index template' do
+          expect(response).to render_template(:index)
+        end
+      end
+
+      context 'and no products are present' do
+        it 'does not assigns @products' do
+          expect(assigns(:products)).to eq([])
+        end
+
+        it 'renders the index template' do
+          expect(response).to render_template(:index)
+        end
+      end
+    end
+
+    context 'when NO user is logged in' do
+      before { get :index }
+      it 'does not render the index template' do
+        expect(response).not_to render_template(:index)
+      end
+
+      it do
+        expect(response).to have_http_status(302)
+      end
+
+      it 'redirect to login page' do
+        expect(subject).to redirect_to('/users/sign_in')
+      end
+    end
+  end
+end
+ 
