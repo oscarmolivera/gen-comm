084d9b543006b2c7294b5c6f16963c66d833bf83
Oscar M Olivera <oscarmolivera@gmail.com>
2020-08-11 17:53:05 -0400

    ProductsController: CREATE close #161

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,5 @@
 class ProductsController < ApplicationController
+  skip_before_action :verify_authenticity_token, only: %i[create]
 
   def index
     @products = Product.all
@@ -6,5 +7,22 @@ class ProductsController < ApplicationController
 
   def new
     @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+    respond_to do |format|
+      if @product.save
+        format.js
+      else
+        format.js { render :new, status: :unauthorized }
+      end
+    end
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:name, :description, :existence, :price, :image, :category_id, :supplier_id)
   end
 end
