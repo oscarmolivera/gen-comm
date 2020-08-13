015c04875a9bdb50035f837010c00cf0625113fe
Oscar M Olivera <oscarmolivera@gmail.com>
2020-08-13 19:30:44 -0400

    AppConfig: GoolgeCloud CarrierWave Access Config close #173

---
+++ config/initializers/carrier_wave.rb
@@ -0,0 +1,8 @@
+CarrierWave.configure do |config|
+  config.fog_credentials = {
+    provider: 'Google',
+    google_storage_access_key_id: Rails.application.credentials.google[:access_key_id],
+    google_storage_secret_access_key: Rails.application.credentials.google[:secret_access_key]
+  }
+  config.fog_directory = 'gc-gen-comm-storage'
+end
