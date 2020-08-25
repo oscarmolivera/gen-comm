628d06c5fe9ac958c1bd817846e0ea35d499cf68
Oscar M Olivera <oscarmolivera@gmail.com>
2020-08-24 20:29:28 -0400

    SalesController: Edit New

--- app/controllers/sales_controller.rb
+++ app/controllers/sales_controller.rb
@@ -2,4 +2,13 @@ class SalesController < ApplicationController
   def index
     @sales = Sale.all
   end
+
+  def new
+    @sale = Sale.create(amount: 0.0)
+    redirect_to edit_sale_path(@sale)
+  end
+
+  def edit
+    @products_sale = @sale.sale_details
+  end
 end
