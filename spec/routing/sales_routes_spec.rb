ab12cf6103cf9d338fa03da46202e47d01667de6
Oscar M Olivera <oscarmolivera@gmail.com>
2020-09-09 20:15:42 -0300

    SalesRoutes-Spec: except (SHOW UPDATE)

--- spec/routing/sales_routes_spec.rb
+++ spec/routing/sales_routes_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe 'Sales Controller', type: :routing do
 
   it { expect(get: sales_path).to route_to('sales#index') }
 
-  it { expect(get: sale_path(id: 1)).to route_to('sales#show', id: '1') }
+  it { expect(get: sale_path(id: 1)).not_to route_to('sales#show', id: '1') }
 
   it { expect(get: new_sale_path(id: 1)).to route_to('sales#new', id: '1') }
   
@@ -12,9 +12,9 @@ RSpec.describe 'Sales Controller', type: :routing do
 
   it { expect(get: edit_sale_path(id: 1)).to route_to('sales#edit', id: '1') }
 
-  it { expect(patch: sale_path(id: 1)).to route_to('sales#update', id: '1') }
+  it { expect(patch: sale_path(id: 1)).not_to route_to('sales#update', id: '1') }
 
-  it { expect(put: sale_path(id: 1)).to route_to('sales#update', id: '1') }
+  it { expect(put: sale_path(id: 1)).not_to route_to('sales#update', id: '1') }
 
   it { expect(delete: sale_path(id: 1)).to route_to('sales#destroy', id: '1')}
 end
